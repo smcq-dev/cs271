e9f52fb76e36458cec1b3a9cdf6a6414e54d77e4
Sean McQuillan <mcquills@oregonstate.edu>
2025-09-27 15:24:41 -0700

    Cploration c00 complete.

---
+++ cplorations/c00/main.c
@@ -0,0 +1,13 @@
+/**************************************** 
+ * C-ploration 0 for CS 271 
+ *   
+ * Sean McQuillan
+ * FALL 2025
+ *****************************************/
+#include <stdio.h>
+int main() {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}

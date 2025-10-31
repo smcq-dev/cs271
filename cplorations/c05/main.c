ad685611a2d5bfb660eb5c8d9784d9ddf2e4b38b
Sean McQuillan <mcquills@oregonstate.edu>
2025-10-31 14:43:11 -0700

    Cploration 5 complete.

---
+++ cplorations/c05/main.c
@@ -0,0 +1,30 @@
+/****************************************
+ * C-ploration 4 for CS 271
+ * 
+ * [NAME] Sean McQuillan
+ * [TERM] FALL 2025
+ * 
+ ****************************************/
+#include "parser.h"
+
+int main(int argc, const char *argv[])
+{		
+
+	if (argc != 2) {
+        printf("Usage: %s [filename]\n", argv[0]);        exit(EXIT_FAILURE);
+    }
+
+    FILE *fin = fopen( argv[1], "r" );
+
+    if (fin == NULL) {
+        perror("Unable to open file!");
+        exit(EXIT_FAILURE);
+    }
+
+    parse(fin);
+    fclose(fin);
+
+			
+}
+
+
---
+++ cplorations/c05/parser.h
@@ -0,0 +1,26 @@
+/****************************************
+ * C-ploration 4 for CS 271
+ * 
+ * [NAME] SEAN MCQUILLAN
+ * [TERM] FALL 2025
+ * 
+ ****************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define MAX_LINE_LENGTH  200
+
+/** function prototypes **/
+char *strip(char *s);
+
+void parse(FILE * file);
+
+bool is_Atype(const char *);
+
+bool is_label(const char *);
+
+bool is_Ctype(const char *);
+
